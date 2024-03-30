354286df17079c83cca3808fa666268569477240
Ekuspreet <ekuspreetsingh2@gmail.com>
2024-03-30 23:48:24 +0530

    added a bunch of features, conditional rendering and meet expiry

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,21 @@
+let activeTab = "";
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  activeTab = tabs[0].url;
+});
+
 var extension = document.getElementById('extension');
+function isGoogleMeetUrl(url) {
+  // Regular expression pattern to match Google Meet URLs
+  return url.includes("meet.google.com");
+}
+
+function displayFeedback(){
+  extension.append((() => { const h2Element = document.createElement('h2'); h2Element.style.color = "#39a78e"; h2Element.innerHTML = 'Link Added Successfully'; return h2Element; })());
+  setTimeout(() => {
+    extension.removeChild(extension.lastChild)
+  }, 2000)
+}
+
 
 async function getAllRooms() {
   try {
@@ -21,11 +38,11 @@ async function getAllRooms() {
 
 
 
-function createRoom(roomName, createTime, roomURL){
+function createRoom(roomName, roomExp, roomURL) {
 
   const requestBody = {
     name: roomName,
-    time: createTime,
+    time: roomExp,
     url: roomURL
   }
 
@@ -36,51 +53,61 @@ function createRoom(roomName, createTime, roomURL){
     },
     body: JSON.stringify(requestBody)
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('POST request successful:', data);
-  })
-  .catch(error => {
-    console.error('There was a problem with the POST request:', error);
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('POST request successful:', data);
+      displayFeedback();
+    })
+    .catch(error => {
+      console.error('There was a problem with the POST request:', error);
+    });
 }
 
-function newMeetRoom(){
+function newMeetRoom() {
   extension.innerHTML = '';
 
   var roomName = document.createElement('input');
   roomName.type = 'text';
-  roomName.placeholder = 'Name';
+  roomName.placeholder = 'Room Name';
 
   var roomURL = document.createElement('input');
-  roomURL.type = 'text';
-  roomURL.placeholder = 'URL';
+  roomURL.style.display = "None"
+  roomURL.value = activeTab;
+  console.log(roomURL)
+
+  var roomExp = document.createElement('input');
+  roomExp.type = 'number';
+  roomExp.placeholder = 'Expiry Time (in Hrs)';
 
   var button = document.createElement('button');
   button.innerHTML = 'Create Room';
-  button.addEventListener('click', function() {
-    if((roomName.value != "") && (roomURL.value != "")){
-      const currentDate = new Date();
-      const formattedDate = currentDate.toISOString();
-      createRoom(roomName.value, formattedDate, roomURL.value)
-    }
-  });
+  button.addEventListener('click', function () {
+    if ((roomName.value != "") && (roomExp.value != "")) {
+      createRoom(roomName.value, roomExp, roomURL.value)
+     
+  }});
 
   var goBack = document.createElement('button');
-  goBack.innerHTML = '<<';
-  goBack.addEventListener('click', function() {
+  goBack.innerHTML = 'Go Back';
+
+  goBack.id = "backbtn"
+  goBack.addEventListener('click', function () {
+    extension.classList.remove("inputpage")
     home()
   });
-
+  extension.classList.add("inputpage")
   extension.appendChild(goBack);
   extension.appendChild(roomName);
   extension.appendChild(roomURL);
+  extension.appendChild(roomExp);
   extension.appendChild(button);
+
+
 }
 
 function chatMeetList() {
@@ -89,41 +116,40 @@ function chatMeetList() {
   getAllRooms()
     .then(rooms => {
       roomList = rooms;
-      
+
       extension.innerHTML = '';
 
+      extension.classList.add("urlpage")
+
+
       var goBack = document.createElement('button');
-      goBack.innerHTML = '<<';
-      goBack.addEventListener('click', function() {
+      goBack.innerHTML = 'Go Back';
+
+      goBack.id = "backbtn"
+      goBack.addEventListener('click', function () {
+        extension.classList.remove("urlpage")
+
         home();
       });
 
       extension.appendChild(goBack);
-
-      roomList.forEach(function(item) {
+      var roomlist = document.createElement('div');
+      roomlist.classList.add("roomlist")
+      roomList.forEach(function (item) {
         console.log(item)
-        var link = document.createElement('a');
+        var link = document.createElement('button');
         link.innerHTML = item.name;
         link.href = item.url;
-        link.style.backgroundColor = '#000'; // Green background color
-        link.style.border = 'none';
-        link.style.color = 'white';
-        link.style.padding = '8px 16px'; // Adjust padding to make the button smaller
-        link.style.textAlign = 'center';
-        link.style.textDecoration = 'none';
-        link.style.display = 'flex'; // Set display to block
-        link.style.flexWrap = 'wrap';
-        link.style.fontSize = '14px'; // Adjust font size to make the button smaller
-        link.style.margin = '10px 0'; // Add margin to separate buttons vertically
-        link.style.cursor = 'pointer';
-        link.style.borderRadius = '4px';
 
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent the default action of clicking a link
-            window.open(item.url);
+
+        link.addEventListener('click', function (event) {
+          event.preventDefault(); // Prevent the default action of clicking a link
+          window.open(item.url);
         });
-        extension.appendChild(link);
+        roomlist.appendChild(link);
       });
+      console.log(roomlist)
+      extension.appendChild(roomlist)
     })
     .catch(error => {
       console.error('Error fetching rooms:', error);
@@ -131,18 +157,33 @@ function chatMeetList() {
 }
 
 
-function home(){
-  extension.innerHTML='';
+function home() {
+  extension.innerHTML = '';
 
   var NewRoom = document.createElement('button')
-  NewRoom.innerHTML = "New Room"
-  NewRoom.addEventListener('click', function() {
-    newMeetRoom()
+  if (isGoogleMeetUrl(activeTab)) {
+    NewRoom.innerHTML = "New Room"
+
+  } else {
+    NewRoom.innerHTML = "Create Meet"
+
+  }
+
+  NewRoom.addEventListener('click', function () {
+    if (isGoogleMeetUrl(activeTab)) {
+
+      newMeetRoom()
+
+    }
+    else {
+
+      chrome.tabs.create({ url: "https://meet.google.com/new" });
+    }
   })
 
   var AllRooms = document.createElement('button')
   AllRooms.innerHTML = "All Rooms"
-  AllRooms.addEventListener('click', function() {
+  AllRooms.addEventListener('click', function () {
     chatMeetList()
   })
 
@@ -150,6 +191,8 @@ function home(){
   extension.appendChild(AllRooms)
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   home()
 });
+
+
